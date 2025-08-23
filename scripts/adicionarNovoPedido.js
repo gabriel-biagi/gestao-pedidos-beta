9037265c213e2be68652ebb15ed4c14bbbf5fa94
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2025-08-23 14:17:24 -0400

    Primeiros arquivos JS sendo criados e lógica de adicionar pedido sendo implementada

---
+++ scripts/adicionarNovoPedido.js
@@ -0,0 +1,3 @@
+export function adicionarNovoPedido(cliente, itens, copo) {
+    console.log(`Pedido de ${cliente} teve o copo do tamanho ${copo} e os seguintes acompanhamentos registrados: ${itens}`);
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-let itensComanda = [];
+import { adicionarNovoPedido } from './adicionarNovoPedido.js';
+
+document.querySelector('.submit-btn-add').addEventListener('click', (e) => {
+    e.preventDefault();
+    const nomeCliente = document.getElementById('cliente').value;
+    const checkboxes = document.querySelectorAll('.acompanhamento-items input[type="checkbox"]');
+    const acompanhamentosSelecionados = [];
+    
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            acompanhamentosSelecionados.push(checkbox.value);
+        }
+    });
+    adicionarNovoPedido(nomeCliente, acompanhamentosSelecionados);
+});
