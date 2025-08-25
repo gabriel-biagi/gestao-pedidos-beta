47cbfbce5dce7291be639ccc6e253791403410be
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2025-08-24 20:17:20 -0400

    Remoção dos Scripts antigos, iniciando nova lógica organizada de Calculo de pedido Final

--- scripts/adicionarNovoPedido.js
+++
@@ -1,3 +0,0 @@
-export function adicionarNovoPedido(cliente, itens, copo) {
-    console.log(`Pedido de ${cliente} teve o copo do tamanho ${copo} e os seguintes acompanhamentos registrados: ${itens}`);
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,53 @@
-import { adicionarNovoPedido } from './adicionarNovoPedido.js';
 
-document.querySelector('.submit-btn-add').addEventListener('click', (e) => {
-    e.preventDefault();
-    const nomeCliente = document.getElementById('cliente').value;
-    const checkboxes = document.querySelectorAll('.acompanhamento-items input[type="checkbox"]');
-    const acompanhamentosSelecionados = [];
-    
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            acompanhamentosSelecionados.push(checkbox.value);
-        }
-    });
-    adicionarNovoPedido(nomeCliente, acompanhamentosSelecionados);
-});
+/*------------------------Lógica da Box Delivery-----------------------*/
+function verificarDelivery() {
+    const enderecoContainer = document.querySelector('.form-group-endereco');
+    if (checkboxDelivery.checked) {
+        enderecoContainer.classList.remove('hidden');
+    } else {
+        enderecoContainer.classList.add('hidden');
+    }
+}
+const checkboxDelivery = document.querySelector('input[name="delivery"]');
+checkboxDelivery.addEventListener('change', verificarDelivery);
+
+
+function verificaAcompanhamentos() {
+    const checkboxAcompanhamentosPagos = document.querySelectorAll('input[name="acompanhamentos_pagos"]');
+
+    checkboxAcompanhamentosPagos.forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            
+        })
+    })
+}
+
+
+/*-----------------Lógica que calcula o Preço Total---------------------*/
+const valorCopos = {
+    '300ml': 16.00,
+    '400ml': 18.00,
+    '550ml': 20.00,
+    '770ml': 25.00
+}
+
+const valorAcompanhamentosPagos = {
+    'nutella': 7.00,
+    'creme de coco bianco': 5.00,
+    'morango': 3.00,
+    'kiwi': 2.00,
+    'creme de ninho': 5.00,
+    'creme de amendoim': 5.00,
+    'ovomaltine': 4.00,
+    'bombom': 2.00,
+    'mms': 2.00,
+    'mel': 2.00,
+    'chocoball': 2.00
+}
+
+let precoTotal;
+let copoSelecionado = null;
+
+function calcularPrecoTotal(copoSelecionado, valorCopos, valorAcompanhamentosPagos, valorDelivery, precoTotal) {
+
+}
