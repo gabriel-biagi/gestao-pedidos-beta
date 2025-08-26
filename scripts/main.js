0bb4ff47a4ba0124ac5dbf55138445864518ff48
gabriel-biagi <gabrielfelipebiagi@gmail.com>
2025-08-25 21:31:20 -0400

    Finalização da Lógica de Soma de Preço dos Produtos

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+const precoTotalElement = document.getElementById('preco-total');
 
 /*------------------------Lógica da Box Delivery-----------------------*/
 function verificarDelivery() {
@@ -7,23 +8,58 @@ function verificarDelivery() {
     } else {
         enderecoContainer.classList.add('hidden');
     }
+
+    calcularPrecoTotal();
 }
 const checkboxDelivery = document.querySelector('input[name="delivery"]');
 checkboxDelivery.addEventListener('change', verificarDelivery);
 
 
-function verificaAcompanhamentos() {
-    const checkboxAcompanhamentosPagos = document.querySelectorAll('input[name="acompanhamentos_pagos"]');
+//-----------------------Lógica dos Copos------------------------------
+let copoSelecionado = null;
+const copos = document.querySelectorAll('.size-btn');
 
-    checkboxAcompanhamentosPagos.forEach(checkbox => {
-        checkbox.addEventListener('change', (e) => {
-            
-        })
+function atualizaCopo() {
+    copos.forEach(copo => {
+        copo.classList.remove('selected');
     })
+    this.classList.add('selected');
+    copoSelecionado = this;
+
+    console.log(copoSelecionado.dataset.size);
+
+    calcularPrecoTotal();
+}
+
+copos.forEach(copo => {
+    copo.addEventListener('click', atualizaCopo);
+})
+
+//------------------------Lógica dos Acompanhamentos--------------------
+let acompanhamentosSelecionados = []
+
+
+const checkboxes = document.querySelectorAll('input[name="acompanhamentos_pagos"]');
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', atualizarAcompanhamentos);
+})
+
+function atualizarAcompanhamentos() {
+    acompanhamentosSelecionados = [];
+    const checkboxesMarcados = document.querySelectorAll('input[name="acompanhamentos_pagos"]:checked');
+    
+    checkboxesMarcados.forEach(checkbox => {
+        acompanhamentosSelecionados.push(checkbox.value);
+    });
+    calcularPrecoTotal();
+    console.log(acompanhamentosSelecionados);
+
 }
 
 
+
 /*-----------------Lógica que calcula o Preço Total---------------------*/
+const precoDelivery = 5.00;
 const valorCopos = {
     '300ml': 16.00,
     '400ml': 18.00,
@@ -45,9 +81,35 @@ const valorAcompanhamentosPagos = {
     'chocoball': 2.00
 }
 
-let precoTotal;
-let copoSelecionado = null;
 
-function calcularPrecoTotal(copoSelecionado, valorCopos, valorAcompanhamentosPagos, valorDelivery, precoTotal) {
+function calcularPrecoTotal() {
+    let precoTotal = 0;
 
+    if (copoSelecionado === null) {
+        precoTotalElement.textContent = `R$ ${precoTotal.toFixed(2).replace('.', ',')}`;
+        return
+    }
+    const tamanhoDoCopo = copoSelecionado.dataset.size;
+    const precoDoCopo = valorCopos[tamanhoDoCopo];
+    precoTotal += precoDoCopo;
+
+    let precoAcompanhamentos = 0;
+    acompanhamentosSelecionados.forEach(acompanhamento => {
+        if (valorAcompanhamentosPagos[acompanhamento]) {
+            precoAcompanhamentos += valorAcompanhamentosPagos[acompanhamento];
+        }
+    })
+    precoTotal += precoAcompanhamentos;
+
+    if (checkboxDelivery.checked) {
+        precoTotal += precoDelivery;
+    }
+    console.log(precoTotal);
+
+    precoTotalElement.textContent = `R$ ${precoTotal.toFixed(2).replace('.', ',')}`;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    verificarDelivery();
+    calcularPrecoTotal();
+});
